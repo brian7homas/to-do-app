80762b605bbfb2d5d785d40acdb85871f1dd714d
Brian Thomas <btdeveloper125@gmail.com>
2021-06-24 14:16:11 -0600

    This commit adds the crossout element to DOM

--- scripts-jquery.js
+++ scripts-jquery.js
@@ -1,27 +1,36 @@
 function newItem(){
+  //create and validate text value
   let li = $('<li class="list-item"></li>');
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
   let inputValue = $('input').val()
   let text = li.append(document.createTextNode(inputValue))
-  
-  
   if(inputValue === ''){
     alert("You must write something");
   }else{
     let list = $('#list');
     list.append(text);
   }
-  
+  //functions
   function crossOut(){
     li.toggleClass('strike')
   }
+  function deleteListItem(){
+    li.addClass('delete')
+  }
   
-  $('.list-item').dblclick(function(){
+  //events
+  li.dblclick(function(){
+    console.log('double click')
     crossOut()
   });
-  
-  //3
-  $('#list').append(document.createElement('crossOutButton'));
-  $('crossOutButton').append(document.createTextNode("X"));
-  $('crossOutButton').appendTo('.list-item');
+  crossOutButton.click(function(){
+    console.log('delete');
+    deleteListItem()
+  })
+  //build and insert close button element and text node containing x  
+  crossOutButton.append(document.createTextNode("X"));
+  li.append(crossOutButton);
+  //make list items sortable
+  $('#list').sortable();
 }
 
