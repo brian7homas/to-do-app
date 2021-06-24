55f21c06813cbe809a758e6641cc164764d94edb
Brian Thomas <btdeveloper125@gmail.com>
2021-06-24 12:07:34 -0600

    This commit adds validation to list items created

--- scripts-jquery.js
+++ scripts-jquery.js
@@ -1,7 +1,15 @@
 function newItem(){
-  let li = $('#list');
+  let li = $('<li></li>');
   let inputValue = $('input').val()
   let text = li.append(document.createTextNode(inputValue))
-  li.append(text)
+  
+  
+  if(inputValue === ''){
+    alert("You must write something");
+  }else{
+    let list = $('#list');
+    list.append(text);
+    console.log(text)
+  }
 }
 
